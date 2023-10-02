574dee87bd5fe711647ccfa9c03ea03304d54864
Kaitlyn Williamson <kaitlyn.shae28@gmail.com>
2023-10-01 20:09:30 -0400

    y

--- main.js
+++ main.js
@@ -3,10 +3,32 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+const heartButtons = document.querySelectorAll('.like');
 
-
-
-
+heartButtons.forEach(heartButton => {
+  heartButton.addEventListener('click', () => {
+    const isHearted = heartButton.classList.contains('activated-heart');
+    mimicServerCall()
+      .then(() => {
+        if (isHearted) {
+          heartButton.innerHTML = EMPTY_HEART;
+          heartButton.classList.remove('activated-heart');
+        } else {
+          heartButton.innerHTML = FULL_HEART;
+          heartButton.classList.add('activated-heart');
+        }
+      })
+      .catch(() => {
+        const error = document.getElementById('modal');
+        const errorMessage = document.getElementById('modal-message');
+        errorMessage.textContent = "Error, please try again.";
+        error.classList.remove('hidden');
+        setTimeout(() => {
+          modal.classList.add('hidden');
+        }, 3000);
+      })
+    })      
+})
 //------------------------------------------------------------------------------
 // Don't change the code below: this function mocks the server response
 //------------------------------------------------------------------------------
